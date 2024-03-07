7643e0fcf828ca5c6a2bf8b398e59c3a99724062
Henry Lee <henryleeworld@gmail.com>
2024-03-08 00:05:11 +0800

    feat:初始化

---
+++ lang/vendor/laravel2step/zh_TW/laravel-verification.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel 2 Step Verification Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'title'                         => '兩步驟驗證',
+    'subtitle'                      => '驗證是必填',
+    'titleFailed'                   => '驗證失敗',
+    'titlePassed'                   => '好消息！',
+
+    'inputAlt1'                     => '代碼輸入 1',
+    'inputAlt2'                     => '代碼輸入 2',
+    'inputAlt3'                     => '代碼輸入 3',
+    'inputAlt4'                     => '代碼輸入 4',
+
+    'verifyButton'                  => '驗證',
+    'attemptsRemaining'             => '剩餘嘗試次數|剩餘嘗試次數',
+    'missingCode'                   => '沒有收到驗證碼？',
+
+    'exceededTitle'                 => '驗證嘗試次數超過',
+    'lockedUntil'                   => '帳戶被鎖定直到：',
+    'tryAgainIn'                    => '再試一次',
+    'returnButton'                  => '返回首頁',
+
+    'verificationEmailSubject'      => '驗證是必填',
+    'verificationEmailGreeting'     => '您好 :username',
+    'verificationEmailMessage'      => '您的帳戶兩步驟驗證碼是：',
+    'verificationEmailButton'       => '立即驗證',
+
+    'verificationEmailSuccess'      => '成功！',
+    'verificationEmailSentMsg'      => '驗證電子郵件已發送！',
+
+    'verificationWarningTitle'      => '警告！',
+    'verificationWarningMessage'    => '這是您的帳戶鎖定 :hours 小時之前的最後一次嘗試。',
+
+    'verificationLockedTitle'       => '哎喲！',
+    'verificationLockedMessage'     => '帳戶被鎖定！',
+
+    'verificationModalConfBtn'      => '好的',
+
+];
