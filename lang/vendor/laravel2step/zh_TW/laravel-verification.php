68c910bec9fc68ed571a545617bb138a700ff3fe
Henry Lee <henryleeworld@gmail.com>
2025-05-30 07:07:20 +0800

    feat:更新最新資料

--- lang/vendor/laravel2step/zh_TW/laravel-verification.php
+++ lang/vendor/laravel2step/zh_TW/laravel-verification.php
@@ -22,23 +22,23 @@ return [
     'missingCode'                   => '沒有收到驗證碼？',
 
     'exceededTitle'                 => '驗證嘗試次數超過',
-    'lockedUntil'                   => '帳戶被鎖定直到：',
+    'lockedUntil'                   => '帳號被鎖定直到：',
     'tryAgainIn'                    => '再試一次',
     'returnButton'                  => '返回首頁',
 
     'verificationEmailSubject'      => '驗證是必填',
     'verificationEmailGreeting'     => '您好 :username',
-    'verificationEmailMessage'      => '您的帳戶兩步驟驗證碼是：',
+    'verificationEmailMessage'      => '您的帳號兩步驟驗證碼是：',
     'verificationEmailButton'       => '立即驗證',
 
     'verificationEmailSuccess'      => '成功！',
     'verificationEmailSentMsg'      => '驗證電子郵件已發送！',
 
     'verificationWarningTitle'      => '警告！',
-    'verificationWarningMessage'    => '這是您的帳戶鎖定 :hours 小時之前的最後一次嘗試。',
+    'verificationWarningMessage'    => '這是您的帳號鎖定 :hours 小時之前的最後一次嘗試。',
 
     'verificationLockedTitle'       => '哎喲！',
-    'verificationLockedMessage'     => '帳戶被鎖定！',
+    'verificationLockedMessage'     => '帳號被鎖定！',
 
     'verificationModalConfBtn'      => '好的',
 
